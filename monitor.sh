8bd14d66f3ee086c1c3867ff5788d610266b56c7
Emil Englesson <englesson.emil@gmail.com>
2025-02-03 00:20:17 +0100

    Made the script a bit more robust

--- monitor.sh
+++ monitor.sh
@@ -8,16 +8,24 @@ if [[ -z "$QBIT_URL" || -z "$QBIT_USER" || -z "$QBIT_PASS" || -z "$FORWARDED_POR
 fi
 
 push_port () {
-  local port=$(cat $FORWARDED_PORT_PATH)
+  local port
+  port=$(cat "$FORWARDED_PORT_PATH")
   echo "Detected port change: $port"
-  update_port port
+  update_port "$QBIT_URL" "$QBIT_USER" "$QBIT_PASS" "$port"
 }
 
 while true; do
-  if [ -f $FORWARDED_PORT_PATH ]; then
+  if [ -f "$FORWARDED_PORT_PATH" ]; then
     push_port
-    inotifywait -mq -e close_write $FORWARDED_PORT_PATH | while read _change; do
-      push_port
+    inotifywait -mq -e close_write,delete_self "$FORWARDED_PORT_PATH" | while read _file event; do
+      case "$event" in
+        CLOSE_WRITE,CLOSE)
+          push_port  # Only read the file if it was modified
+          ;;
+        DELETE_SELF)
+          break  # Exit monitoring if the file is deleted
+          ;;
+      esac
     done
   else
     echo "$FORWARDED_PORT_PATH not found, sleeping for 10 seconds"
--- qbit.sh
+++ qbit.sh
@@ -3,33 +3,43 @@
 COOKIES="/tmp/cookies.txt"
 
 login () {
-  local login_url="${QBIT_URL}/api/v2/auth/login"
-  local login_data="username=$QBIT_USER&password=$QBIT_PASS"
+  local qbit_url=$1
+  local qbit_user=$2
+  local qbit_pass=$3
+
+  local login_url="${qbit_url}/api/v2/auth/login"
+  local login_data="username=${qbit_user}&password=${qbit_pass}"
   local status
-  status=$(curl --silent --cookie-jar $COOKIES --data ${login_data} ${login_url} -o /dev/null -w "%{http_code}")
-  echo $status
+  status=$(curl --silent --cookie-jar $COOKIES --data "${login_data}" "${login_url}" -o /dev/null -w "%{http_code}")
+  echo "$status"
 }
 
 update_preferences () {
-  local preference_url="${QBIT_URL}/api/v2/app/setPreferences"
-  local preference_data="json={\"listen_port\":\"$1\"}"
+  local qbit_url=$1
+  local port=$2
+
+  local preference_url="${qbit_url}/api/v2/app/setPreferences"
+  local preference_data="json={\"listen_port\":\"$port\"}"
   local status
-  status=$(curl --silent --cookie $COOKIES --data "${preference_data}" ${preference_url} -o /dev/null -w "%{http_code}")
-  echo $status
+  status=$(curl --silent --cookie $COOKIES --data "${preference_data}" "${preference_url}" -o /dev/null -w "%{http_code}")
+  echo "$status"
 }
 
 update_port () {
-  local port=$1
+  local qbit_url=$1
+  local qbit_user=$2
+  local qbit_pass=$3
+  local port=$4
 
   local status
-  status=$(update_preferences $port)
+  status=$(update_preferences "$qbit_url" "$port")
   if [ "$status" -eq 200 ]; then
     echo "qBittorrent port updated to $port"
   else
-    status=$(login)
+    status=$(login "$qbit_url" "$qbit_user" "$qbit_pass" "$port")
     if [ "$status" -eq 200 ]; then
       echo "Logged into qbittorrent"
-      status=$(update_preferences $port)
+      status=$(update_preferences "$qbit_url" "$port")
       if [ "$status" -eq 200 ]; then
         echo "Port updated to $port"
       else
